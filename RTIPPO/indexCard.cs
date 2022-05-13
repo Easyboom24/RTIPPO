dd4ffc3a7d903c3b842b752a359030f997910d1b
Easyboom24 <yaro_99_99@bk.ru>
2022-05-13 15:54:24 +0500

    Реализация. Режим просмотра при открытии карточки.

--- RTIPPO/indexCard.cs
+++ RTIPPO/indexCard.cs
@@ -17,7 +17,6 @@ namespace RTIPPO
         public indexCard()
         {
             InitializeComponent();
-
         }
 
         private void edit_Click(object sender, EventArgs e)
@@ -62,45 +61,79 @@ namespace RTIPPO
             if(application != null) 
             { 
                 regNum.Text =  application.Application_Number.ToString();
+                regNum.ReadOnly = true;
                 dateApplication.Value =  application.Filling_Date.ToDateTime(new TimeOnly(0));
+                dateApplication.Enabled = false;
                 trappingOrg.Text = application?.Organization?.Organization_Name ?? "";
+                trappingOrg.ReadOnly = true;
                 status.Text = application?.Status?.Status_Name ?? "";
+                status.Enabled = false;
                 dateStatus.Value = application.Status_Date.ToDateTime(new TimeOnly(0));
+                dateStatus.Enabled = false;
                 place.Text = application?.Locality?.Locality_Name ?? "";
+                place.ReadOnly = true;
             
                 habitat.Text = application?.Animal?.Habitat ?? "";
+                habitat.ReadOnly = true;
                 category.Text = application?.Animal?.Animal_Category?.Category_Name ?? "";
+                category.Enabled = false;
                 wool.Text = application?.Animal?.Animal_Wool?.Wool_Name ?? "";
+                wool.Enabled = false;
                 ears.Text = application?.Animal?.Ears ?? "";
+                ears.ReadOnly = true;
                 sizeAnimal.Text = application?.Animal?.Animal_Size?.Size_Name ?? "";
+                sizeAnimal.ReadOnly = true;
                 color.Text = application?.Animal?.Color ?? "";
+                color.ReadOnly = true;
                 tail.Text = application?.Animal?.Tail ?? "";
+                tail.ReadOnly = true;
                 signs.Text = application?.Animal?.Signs ?? "";
+                signs.ReadOnly = true;
                 urgency.Text = application?.Urgency?.Urgency_Name ?? "";
+                urgency.Enabled = false;
                 reason.Text = application?.Reason ?? "";
+                reason.ReadOnly = true;
                 sex.Text = application?.Animal?.Animal_Sex?.Sex_Name ?? "";
+                sex.Enabled = false;
 
                 foreach (var status in application?.Status_History)
                     history_status_textbox.Text += status.Status.Status_Name+ "-" + status.Current_Date_Status + Environment.NewLine;
 
+                history_status_textbox.ReadOnly = true;
+
                 surname.Text = application?.Applicant?.Applicant_Surname ?? "";
+                surname.ReadOnly = true;
                 firstname.Text = application?.Applicant?.Applicant_Firstname ?? "";
+                firstname.ReadOnly = true;
                 patronymic.Text = application?.Applicant?.Applicant_Patronymic ?? "";
+                patronymic.ReadOnly = true;
                 phone.Text = application?.Applicant?.Applicant_Phone ?? "";
+                phone.ReadOnly = true;
                 email.Text = application?.Applicant?.Applicant_Email ?? "";
+                email.ReadOnly = true;
                 address.Text = application?.Applicant?.Applicant_Address ?? "";
+                address.ReadOnly = true;
 
                 applicantCategory.Text = application?.Applicant?.Organization?.Type_Enterprise.Type_Enterprise_Name ?? "Физ. лицо";
-           
+                applicantCategory.Enabled = false;
+
                 INN.Text = application?.Applicant?.Organization?.INN ?? "";
+                INN.ReadOnly = true;
                 KPP.Text = application?.Applicant?.Organization?.KPP ?? "";
+                KPP.ReadOnly = true;
                 name.Text = application?.Applicant?.Organization?.Organization_Name ?? "";
+                name.ReadOnly = true;
                 phoneUr.Text = application?.Applicant?.Organization?.Phone ?? "";
+                phoneUr.ReadOnly = true;
                 //emailUr.Text = application?.Organization?.
                 addressUr.Text = application?.Applicant?.Organization?.Address ?? "";
+                addressUr.ReadOnly = true;
                 surDirector.Text = application?.Applicant?.Organization?.Surname_Director ?? "";
+                surDirector.ReadOnly = true;
                 firDirector.Text = application?.Applicant?.Organization?.Firstname_Director ?? "";
+                firDirector.ReadOnly = true;
                 patDirector.Text = application?.Applicant?.Organization?.Patronymic_Director ?? "";
+                patDirector.ReadOnly = true;
             }
 
         }
